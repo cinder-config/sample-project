92e4409e41481697d33451f6989a672f50475f34
Timothy Zemp <timothy.zemp@gmail.com>
2020-11-18 20:55:44 +0100

    fix failing test
    Signed-off-by: Timothy Zemp <timothy.zemp@gmail.com>

--- src/test/java/ch/uzh/tzemp/ciclassifier/LeadYearTest.java
+++ src/test/java/ch/uzh/tzemp/ciclassifier/LeadYearTest.java
@@ -22,6 +22,6 @@ public class LeadYearTest {
     @Test
     public void failingTest() {
         LeapYear leapYear = new LeapYear();
-        assertFalse(leapYear.isLeapYear(2024));
+        assertFalse(leapYear.isLeapYear(2023));
     }
 }
